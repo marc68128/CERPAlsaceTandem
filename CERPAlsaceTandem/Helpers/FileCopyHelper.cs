3f7e2fcabedb221848713aadaccb0d9275bcc362
Marc Unterseh <Marc Unterseh>
2016-06-30 11:48:17 +0200

    Add FileCopy helper

--- CERPAlsaceTandem/Helpers/FileCopyHelper.cs
+++ CERPAlsaceTandem/Helpers/FileCopyHelper.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CERPAlsaceTandem.Models;
+using CERPAlsaceTandem.ViewModels;
 
 namespace CERPAlsaceTandem.Helpers
 {
-    class FileCopyHelper
+    public static class FileCopyHelper
     {
+        public static async Task<bool> Copy<T>(FileCollection<T> files, PassengerViewModel passenger) where T : FileViewModel
+        {
+            return await Task.Run(() =>
+            {
+                return true;
+            });
+        } 
     }
 }
